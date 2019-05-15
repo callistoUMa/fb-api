a00edcf1545f00961f0e8d20fc7480dac1b9407e
callistoUMa <48031903+callistoUMa@users.noreply.github.com>
2019-05-15 12:47:59 +0200

    Delete spa.js

--- spa.js
+++
@@ -1,7 +0,0 @@
-function changePage(id){
-	var pages = document.getElementsByClassName('content');
-	for(var i = 0; i < pages.length; i++){
-	pages[i].style.display = 'none';}
-	document.getElementById(id).style.display = 'block';
-	return;
-	}
